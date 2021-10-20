8f0250c3f1229500587a0fffe980b4b72469106c
Johannes Scholz <js@vinci-it.com>
2021-10-20 22:33:14 +0200

    syntax

--- ci-scripts/install-pulumi.sh
+++ ci-scripts/install-pulumi.sh
@@ -2,7 +2,7 @@
 
 apk add curl
 curl \
-    -L https://github.com/pulumi/pulumi/releases/download/v3.16.0/pulumi-v3.16.0-linux-x64.tar.gz
+    -L https://github.com/pulumi/pulumi/releases/download/v3.16.0/pulumi-v3.16.0-linux-x64.tar.gz \
     -o pulumi.tar.gz
 
 tar xzf pulumi.tar.gz && \
