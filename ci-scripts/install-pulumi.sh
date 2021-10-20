c94a32ec67b69cc37ba673a5117af10823a6e96a
Johannes Scholz <js@vinci-it.com>
2021-10-20 22:26:28 +0200

    add script to install pulumi

---
+++ ci-scripts/install-pulumi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apk add curl
+curl
+    -L https://github.com/pulumi/pulumi/releases/download/v3.16.0/pulumi-v3.16.0-linux-x64.tar.gz
+    -o pulumi.tar.gz
+
+tar xzf pulumi.tar.gz && \
+    rm -f pulumi.tar.gz && \
+    ln -s /usr/bin/pulumi pulumi
--- ci-scripts/install-pulumictl.sh
+++ ci-scripts/install-pulumictl.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+apk add curl
 curl
     -L https://github.com/pulumi/pulumictl/releases/download/v0.0.28/pulumictl-v0.0.28-darwin-amd64.tar.gz \
     -o pulumictl.tar.gz
 
-tar xzf pulumictl.tar.gz && rm pulumictl.tar.gz && ln -s /usr/bin/pulumictl pulumictl
+tar xzf pulumictl.tar.gz && \
+    rm -f pulumictl.tar.gz && \
+    ln -s /usr/bin/pulumictl pulumictl
