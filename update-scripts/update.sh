00b5cb916e5a96ba5b626ea3aaec2f18281b8f14
Johannes Scholz <js@vinci-it.com>
2022-02-05 01:33:41 +0100

    extract resoures from submodule's resources.go

--- update-scripts/update.sh
+++ update-scripts/update.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+set -e
 
 SCRIPT_ROOT=$(dirname $0)
+PROVIDER_NAME=$(yq '.provider.name' $SCRIPT_ROOT/provider_config.yml)
 
 $SCRIPT_ROOT/extract_resources.py \
-    ~/development/terraform-provider-fortios/fortios/provider.go \
+    ../terraform-provider-$PROVIDER_NAME/$PROVIDER_NAME/provider.go \
     -c $SCRIPT_ROOT/provider_config.yml \
     -o $SCRIPT_ROOT/../provider/resources.go && \
     go fmt $SCRIPT_ROOT/../provider/resources.go
